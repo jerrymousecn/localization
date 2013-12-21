7ee2125e0f1c0cdd1d03d632baac386ba563259d
jerrymousecn <yinxing1999@gmail.com>
2013-12-21 21:50:16 +0800

    Signed-off-by: jerrymousecn <yinxing1999@gmail.com>

---
+++ source/localization2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace localization2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void SetResourceCulture()
+        {
+            // Set the form title text  
+            this.Text = ResourceCulture.GetString("Form1");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set the default language  
+            //ResourceCulture.SetCurrentCulture("en-US");
+            ResourceCulture.SetCurrentCulture("zh-CN");
+
+            this.SetResourceCulture(); 
+        }
+    }
+}
